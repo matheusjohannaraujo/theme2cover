283dca1ff9528653bbcb79703bb071506f1fe1c1
Matheus Johann Araujo <matheus.araujo@recife.pe.gov.br>
2026-09-20 13:05:42 -0300

    #000

--- src/Theme2Cover.php
+++ src/Theme2Cover.php
@@ -666,7 +666,7 @@ class Theme2Cover
                 */
 
                 $font->wrap(
-                    1000
+                    1050
                 );
 
                 $font->lineHeight(
